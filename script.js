43c0ba7e57418ad836f283f785f3df99982854df
GedeonPereira <gedeonpereira427@gmail.com>
2026-05-25 08:53:12 -0300

    eu fiz 8 exercicios

--- script.js
+++ script.js
@@ -1,10 +1,109 @@
-function somar(a, b) {
-  return a + b;
+function nmaior(a, b) {
+  if (typeof a == Number && typeof b == Number) {
+    if (a > b) {
+      return "o primeiro numero é maior"
+    } else {
+      return "o segundo numero é maior"
+    }
+  } else {
+    return "digite um numero"
+  }
 }
 
-function mensagemBoasVindas(nome) {
-  return `Olá, ${nome}!`;
+function nparImpar(a, b) {
+  if (typeof a == Number && typeof b == Number) {
+    if (a % 2 == 0 && b % 2 == 0) {
+      return "ambos sao pares"
+    }
+    if (a % 2 == 0 && b % 2 != 0) {
+      return "o primeiro numero é par e o segundo é impar"
+    }
+    if (a % 2 != 0 && b % 2 == 0){
+      return "o primeiro numero é impar e o segundo é par"
+    }else {
+      return "ambos sao impares"
+    }
+  } else {
+    return "digite um numero"
+  }
 }
 
-module.exports = { somar, mensagemBoasVindas };
-// export default { somar, mensagemBoasVindas };
+function votar(idade) {
+  if (typeof idade == Number) {
+    if (idade >= 18) {
+      return "voce pode e deve votar"
+    }
+    if (idade >= 16 && idade < 18 ) {
+      return "voce pode mais nao precisa votar "
+    }
+    if (idade < 16){
+      return "voce nao pode votar"
+    }else {
+      return "digite numeros"
+    }
+  }
+}
+
+function numero(num) {
+  if (typeof num == Number) {
+    if (num >= 0) {
+      return "o numero é positivo"
+    }
+    if (num == 0 ) {
+      return "o numero é nulo "
+    }
+    if (num < 0){
+      return "o numero é negativo"
+    }else {
+      return "digite um numero"
+    }
+  }
+}
+
+function salario(salario) {
+  if (typeof salario == Number) {
+    if (salario >= 5000) {
+      return "o salario tem um bonus de 5%"
+    }
+    if (salario >= 2000 && salario < 5000 ) {
+      return "o salario tem um bonus de 10%"
+    }
+    if (salario < 2000){
+      return "o salario tem um bonus de 20%"
+    }else {
+      return "digite um numero"
+    }
+  }
+}
+
+function dirigir(idade) {
+  if (typeof idade == Number) {
+    if (idade >= 18) {
+      return "voce pode dirigir"
+    }
+    if (idade < 18) {
+      return "voce não pode dirigir legalmente né"
+    }else {
+      return "digite um numero"
+    }
+  }
+}
+
+function mesSemestre(mes) {
+  if (typeof mes == Number) {
+    if (mes > 0 && mes <= 12) {
+      if (mes <= 6) {
+        return "seu mes esta no primeiro semestre"
+      }
+      if (mes > 6) {
+        return "seu mes esta no segundo semestre"
+      }
+    } else {
+      return "digite um numero que representa um mes"
+    }
+  } else {
+    return "digite um numero"
+  }
+}
+
+module.exports {mesSemestre, dirigir, salario, numero, votar, nparImpar, nmaior};
--- script.test.js
+++ script.test.js
@@ -1,9 +1,41 @@
-const { somar, mensagemBoasVindas } = require('./script');
+const { mesSemestre, dirigir, salario, numero, votar, nparImpar, nmaior} = require('./script');
 
-test('soma dois números', () => {
-  expect(somar(2, 3)).toBe(5);
+test('nmaior', () => {
+  expect(typeof nmaior).toBe("Number");
+  expect(nmaior('6,7')).toBe('o segundo numero é maior')
 });
 
-test('mensagem de boas vindas', () => {
-  expect(mensagemBoasVindas('Diogo')).toBe('Olá, Diogo!');
+test('nparImpar', () => {
+  except(typeof nparImpar).toBe("Number")
+  expect(nparImpar('2,2')).toBe('ambos sao pares');
 });
+
+test('votar', () => {
+  except(typeof votar).toBe("Number");
+  except(votar('18')).toBe('voce pode e deve votar')
+})
+
+test('numero', () => {
+  except(typeof votar).toBe("Number");
+  except(votar('18')).toBe('voce pode e deve votar')
+})
+
+test('salario', () => {
+  except(typeof votar).toBe("Number");
+  except(votar('18')).toBe('voce pode e deve votar')
+})
+
+test('dirigir', () => {
+  except(typeof votar).toBe("Number");
+  except(votar('18')).toBe('voce pode e deve votar')
+})
+
+test('mesSemestre', () => {
+  except(typeof votar).toBe("Number");
+  except(votar('18')).toBe('voce pode e deve votar')
+})
+
+test('votar', () => {
+  except(typeof votar).toBe("Number");
+  except(votar('18')).toBe('voce pode e deve votar')
+})
