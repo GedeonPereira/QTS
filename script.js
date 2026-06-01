c519c0d892bfff7d8e51fb1ad5f702200536ea64
GedeonPereira <gedeonpereira427@gmail.com>
2026-06-01 19:10:53 -0300

    arrumei tudo

--- script.js
+++ script.js
@@ -1,109 +1,171 @@
-function nmaior(a, b) {
-  if (typeof a == Number && typeof b == Number) {
+
+js
+function maiorValor(a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
     if (a > b) {
-      return "o primeiro numero é maior"
-    } else {
-      return "o segundo numero é maior"
+      return `${a} é maior`;
     }
+
+    if (b > a) {
+      return `${b} é maior`;
+    }
+
+    return 'Os valores são iguais';
   } else {
-    return "digite um numero"
+    return 'Digite números';
   }
 }
 
-function nparImpar(a, b) {
-  if (typeof a == Number && typeof b == Number) {
-    if (a % 2 == 0 && b % 2 == 0) {
-      return "ambos sao pares"
-    }
-    if (a % 2 == 0 && b % 2 != 0) {
-      return "o primeiro numero é par e o segundo é impar"
-    }
-    if (a % 2 != 0 && b % 2 == 0){
-      return "o primeiro numero é impar e o segundo é par"
-    }else {
-      return "ambos sao impares"
-    }
+function parOuImpar(a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+    let resultadoA = a % 2 === 0 ? 'par' : 'ímpar';
+    let resultadoB = b % 2 === 0 ? 'par' : 'ímpar';
+
+    return `${a} é ${resultadoA} e ${b} é ${resultadoB}`;
   } else {
-    return "digite um numero"
+    return 'Digite números';
   }
 }
 
 function votar(idade) {
-  if (typeof idade == Number) {
-    if (idade >= 18) {
-      return "voce pode e deve votar"
+  if (typeof idade === 'number') {
+    if (idade < 16) {
+      return 'Não pode votar';
     }
-    if (idade >= 16 && idade < 18 ) {
-      return "voce pode mais nao precisa votar "
+
+    if ((idade >= 16 && idade < 18) || idade >= 70) {
+      return 'Voto opcional';
     }
-    if (idade < 16){
-      return "voce nao pode votar"
-    }else {
-      return "digite numeros"
-    }
+
+    return 'Pode votar';
+  } else {
+    return 'Digite um número';
   }
 }
 
-function numero(num) {
-  if (typeof num == Number) {
-    if (num >= 0) {
-      return "o numero é positivo"
+function verificarNumero(valor) {
+  if (typeof valor === 'number') {
+    if (valor > 0) {
+      return 'Positivo';
     }
-    if (num == 0 ) {
-      return "o numero é nulo "
+
+    if (valor < 0) {
+      return 'Negativo';
     }
-    if (num < 0){
-      return "o numero é negativo"
-    }else {
-      return "digite um numero"
-    }
+
+    return 'Igual a zero';
+  } else {
+    return 'Digite um número';
   }
 }
 
 function salario(salario) {
-  if (typeof salario == Number) {
+  if (typeof salario === 'number') {
     if (salario >= 5000) {
-      return "o salario tem um bonus de 5%"
+      return 'O salário tem um bônus de 5%';
     }
-    if (salario >= 2000 && salario < 5000 ) {
-      return "o salario tem um bonus de 10%"
+
+    if (salario >= 2000 && salario < 5000) {
+      return 'O salário tem um bônus de 10%';
     }
-    if (salario < 2000){
-      return "o salario tem um bonus de 20%"
-    }else {
-      return "digite um numero"
+
+    if (salario < 2000) {
+      return 'O salário tem um bônus de 20%';
     }
+  } else {
+    return 'Digite um número';
   }
 }
 
 function dirigir(idade) {
-  if (typeof idade == Number) {
+  if (typeof idade === 'number') {
     if (idade >= 18) {
-      return "voce pode dirigir"
+      return 'Pode dirigir';
     }
-    if (idade < 18) {
-      return "voce não pode dirigir legalmente né"
-    }else {
-      return "digite um numero"
-    }
-  }
-}
 
-function mesSemestre(mes) {
-  if (typeof mes == Number) {
-    if (mes > 0 && mes <= 12) {
-      if (mes <= 6) {
-        return "seu mes esta no primeiro semestre"
-      }
-      if (mes > 6) {
-        return "seu mes esta no segundo semestre"
-      }
-    } else {
-      return "digite um numero que representa um mes"
-    }
+    return 'Não pode dirigir';
   } else {
-    return "digite um numero"
+    return 'Digite um número';
   }
 }
 
-module.exports {mesSemestre, dirigir, salario, numero, votar, nparImpar, nmaior};
+function semestre(mes) {
+  if (typeof mes === 'string') {
+    mes = mes.toLowerCase();
+
+    if (
+      mes === 'janeiro' ||
+      mes === 'fevereiro' ||
+      mes === 'março' ||
+      mes === 'abril' ||
+      mes === 'maio' ||
+      mes === 'junho'
+    ) {
+      return 'Primeiro semestre';
+    }
+
+    return 'Segundo semestre';
+  } else {
+    return 'Digite um mês válido';
+  }
+}
+
+function vogalOuConsoante(letra) {
+  if (typeof letra === 'string') {
+    letra = letra.toLowerCase();
+
+    if (
+      letra === 'a' ||
+      letra === 'e' ||
+      letra === 'i' ||
+      letra === 'o' ||
+      letra === 'u'
+    ) {
+      return 'Vogal';
+    }
+
+    return 'Consoante';
+  } else {
+    return 'Digite uma letra';
+  }
+}
+
+function media(array) {
+  if (typeof array === 'object') {
+    let soma = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      soma += array[i];
+    }
+
+    return `A média é ${soma / array.length}`;
+  } else {
+    return 'Digite um array';
+  }
+}
+
+function login(usuario, senha) {
+  if (typeof usuario === 'string' && typeof senha === 'string') {
+    if (usuario === 'Admin' && senha === '123') {
+      return 'Conectado com sucesso!';
+    }
+
+    return 'Acesso negado! Usuário ou senha incorretos!';
+  } else {
+    return 'Digite dados válidos';
+  }
+}
+
+module.exports = {
+  maiorValor,
+  parOuImpar,
+  votar,
+  verificarNumero,
+  salario,
+  dirigir,
+  semestre,
+  vogalOuConsoante,
+  media,
+  login
+};
+
